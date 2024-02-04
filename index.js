9d5d2b39c89a56b018148139e82483aa84f5b70c
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2024-02-03 19:29:37 -0600

    minor fixes

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ app.get("/", function (req, res) {
 
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+  return res.json({ greeting: "hello API" });
 });
 
 app.get("/api/:date?", (req, res) => {
-  let calcDate = isNaN(req.params.date)
-    ? req.params.date
-    : parseInt(req.params.date);
-  let currentDate = new Date(calcDate);
-  let errorResponse = { error: `${currentDate}` };
-  let validResponse = {
-    unix: currentDate.getTime(),
-    utc: `${currentDate.toUTCString()}`,
-  };
-  currentDate == "Invalid Date"
-    ? res.json(errorResponse)
-    : res.json(validResponse);
+  if (!req.params.date) {
+    res.json({ unix: new Date().getTime() });
+  } else {
+    const input = parseInt(req.params.date);
+    if (!isNaN(input)) {
+      const current = new Date(input);
+      res.json({ unix: current.getTime(), utc: current.toUTCString() });
+    } else {
+      const current = new Date();
+      res.json({ unix: current.getTime(), utc: current.toUTCString() });
+    }
+  }
 });
 
 // listen for requests :)
